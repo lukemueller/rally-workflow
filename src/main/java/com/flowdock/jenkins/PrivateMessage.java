4642b412f633245da95916539bcd39777e146b3f
Luke Mueller <lmueller@rallydev.com>
2013-06-05 15:19:29 -0600

    Implemented asPostData and tests for PrivateMessage.

--- src/main/java/com/flowdock/jenkins/PrivateMessage.java
+++ src/main/java/com/flowdock/jenkins/PrivateMessage.java
@@ -1,4 +1,14 @@
 package com.flowdock.jenkins;
 
-public class PrivateMessage {
+import java.io.UnsupportedEncodingException;
+
+public class PrivateMessage extends FlowdockMessage {
+    @Override
+    public String asPostData() throws UnsupportedEncodingException {
+        StringBuffer postData = new StringBuffer();
+        postData.append("event=").append("message");
+        postData.append("&content=").append(urlEncode(content));
+
+        return postData.toString();
+    }
 }
--- src/test/java/com/flowdock/jenkins/PrivateMessageTest.java
+++ src/test/java/com/flowdock/jenkins/PrivateMessageTest.java
@@ -1,4 +1,20 @@
 package com.flowdock.jenkins;
 
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 public class PrivateMessageTest {
+
+    @Test
+    public void asPostDataSetsRequiredPostParams() throws UnsupportedEncodingException {
+        PrivateMessage privateMessage = new PrivateMessage();
+        String postData = privateMessage.asPostData();
+
+        assertThat(postData, containsString("event=message&content="));
+    }
+
 }
